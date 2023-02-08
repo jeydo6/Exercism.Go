104f53cdd0e92c2b22024bc13f389053fcc65065
jeydo6 <Deryagin.Valdemar@yandex.ru>
2023-02-08 22:24:12 +0300

    parsing_log_files

---
+++ src/parsing-log-files/parsing_log_files.go
@@ -0,0 +1,47 @@
+package parsinglogfiles
+
+import "regexp"
+
+func IsValidLine(text string) bool {
+	re := regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\]`)
+	return re.MatchString(text)
+}
+
+func SplitLogLine(text string) []string {
+	re := regexp.MustCompile(`<[*~=-]*>`)
+	return re.Split(text, -1)
+}
+
+func CountQuotedPasswords(lines []string) int {
+	re := regexp.MustCompile(`(?i)".*password.*"`)
+
+	count := 0
+	for _, line := range lines {
+		if re.MatchString(line) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func RemoveEndOfLineText(text string) string {
+	re := regexp.MustCompile(`end-of-line\d+`)
+	return re.ReplaceAllString(text, "")
+}
+
+func TagWithUserName(lines []string) []string {
+	re := regexp.MustCompile(`\sUser\s+(\S+)`)
+
+	outlines := make([]string, len(lines))
+	for i, line := range lines {
+		match := re.FindStringSubmatch(line)
+		if len(match) > 1 {
+			outlines[i] = "[USR] " + match[1] + " " + line
+		} else {
+			outlines[i] = line
+		}
+	}
+
+	return outlines
+}
